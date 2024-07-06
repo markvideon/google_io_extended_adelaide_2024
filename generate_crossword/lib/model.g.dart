b5951f4e4f33e3561fd36b38a1883f52ac9559c6
markvideon <mark.videon@gmail.com>
2024-07-06 17:12:12 +0930

    Manage the work queue

--- generate_crossword/lib/model.g.dart
+++ generate_crossword/lib/model.g.dart
@@ -11,6 +11,7 @@ Serializers _$serializers = (new Serializers().toBuilder()
       ..add(CrosswordCharacter.serializer)
       ..add(CrosswordWord.serializer)
       ..add(Location.serializer)
+      ..add(WorkQueue.serializer)
       ..addBuilderFactory(
           const FullType(BuiltList, const [const FullType(CrosswordWord)]),
           () => new ListBuilder<CrosswordWord>())
@@ -19,7 +20,17 @@ Serializers _$serializers = (new Serializers().toBuilder()
             const FullType(Location),
             const FullType(CrosswordCharacter)
           ]),
-          () => new MapBuilder<Location, CrosswordCharacter>()))
+          () => new MapBuilder<Location, CrosswordCharacter>())
+      ..addBuilderFactory(
+          const FullType(BuiltMap,
+              const [const FullType(Location), const FullType(Direction)]),
+          () => new MapBuilder<Location, Direction>())
+      ..addBuilderFactory(
+          const FullType(BuiltSet, const [const FullType(Location)]),
+          () => new SetBuilder<Location>())
+      ..addBuilderFactory(
+          const FullType(BuiltSet, const [const FullType(String)]),
+          () => new SetBuilder<String>()))
     .build();
 Serializer<Location> _$locationSerializer = new _$LocationSerializer();
 Serializer<CrosswordWord> _$crosswordWordSerializer =
@@ -27,6 +38,7 @@ Serializer<CrosswordWord> _$crosswordWordSerializer =
 Serializer<CrosswordCharacter> _$crosswordCharacterSerializer =
     new _$CrosswordCharacterSerializer();
 Serializer<Crossword> _$crosswordSerializer = new _$CrosswordSerializer();
+Serializer<WorkQueue> _$workQueueSerializer = new _$WorkQueueSerializer();
 
 class _$LocationSerializer implements StructuredSerializer<Location> {
   @override
@@ -252,6 +264,77 @@ class _$CrosswordSerializer implements StructuredSerializer<Crossword> {
                 const FullType(Location),
                 const FullType(CrosswordCharacter)
               ]))!);
+          break;
+      }
+    }
+
+    return result.build();
+  }
+}
+
+class _$WorkQueueSerializer implements StructuredSerializer<WorkQueue> {
+  @override
+  final Iterable<Type> types = const [WorkQueue, _$WorkQueue];
+  @override
+  final String wireName = 'WorkQueue';
+
+  @override
+  Iterable<Object?> serialize(Serializers serializers, WorkQueue object,
+      {FullType specifiedType = FullType.unspecified}) {
+    final result = <Object?>[
+      'crossword',
+      serializers.serialize(object.crossword,
+          specifiedType: const FullType(Crossword)),
+      'locationsToTry',
+      serializers.serialize(object.locationsToTry,
+          specifiedType: const FullType(BuiltMap,
+              const [const FullType(Location), const FullType(Direction)])),
+      'badLocations',
+      serializers.serialize(object.badLocations,
+          specifiedType:
+              const FullType(BuiltSet, const [const FullType(Location)])),
+      'candidateWords',
+      serializers.serialize(object.candidateWords,
+          specifiedType:
+              const FullType(BuiltSet, const [const FullType(String)])),
+    ];
+
+    return result;
+  }
+
+  @override
+  WorkQueue deserialize(Serializers serializers, Iterable<Object?> serialized,
+      {FullType specifiedType = FullType.unspecified}) {
+    final result = new WorkQueueBuilder();
+
+    final iterator = serialized.iterator;
+    while (iterator.moveNext()) {
+      final key = iterator.current! as String;
+      iterator.moveNext();
+      final Object? value = iterator.current;
+      switch (key) {
+        case 'crossword':
+          result.crossword.replace(serializers.deserialize(value,
+              specifiedType: const FullType(Crossword))! as Crossword);
+          break;
+        case 'locationsToTry':
+          result.locationsToTry.replace(serializers.deserialize(value,
+              specifiedType: const FullType(BuiltMap, const [
+                const FullType(Location),
+                const FullType(Direction)
+              ]))!);
+          break;
+        case 'badLocations':
+          result.badLocations.replace(serializers.deserialize(value,
+                  specifiedType: const FullType(
+                      BuiltSet, const [const FullType(Location)]))!
+              as BuiltSet<Object?>);
+          break;
+        case 'candidateWords':
+          result.candidateWords.replace(serializers.deserialize(value,
+                  specifiedType:
+                      const FullType(BuiltSet, const [const FullType(String)]))!
+              as BuiltSet<Object?>);
           break;
       }
     }
@@ -745,6 +828,158 @@ class CrosswordBuilder implements Builder<Crossword, CrosswordBuilder> {
       } catch (e) {
         throw new BuiltValueNestedFieldError(
             r'Crossword', _$failedField, e.toString());
+      }
+      rethrow;
+    }
+    replace(_$result);
+    return _$result;
+  }
+}
+
+class _$WorkQueue extends WorkQueue {
+  @override
+  final Crossword crossword;
+  @override
+  final BuiltMap<Location, Direction> locationsToTry;
+  @override
+  final BuiltSet<Location> badLocations;
+  @override
+  final BuiltSet<String> candidateWords;
+
+  factory _$WorkQueue([void Function(WorkQueueBuilder)? updates]) =>
+      (new WorkQueueBuilder()..update(updates))._build();
+
+  _$WorkQueue._(
+      {required this.crossword,
+      required this.locationsToTry,
+      required this.badLocations,
+      required this.candidateWords})
+      : super._() {
+    BuiltValueNullFieldError.checkNotNull(crossword, r'WorkQueue', 'crossword');
+    BuiltValueNullFieldError.checkNotNull(
+        locationsToTry, r'WorkQueue', 'locationsToTry');
+    BuiltValueNullFieldError.checkNotNull(
+        badLocations, r'WorkQueue', 'badLocations');
+    BuiltValueNullFieldError.checkNotNull(
+        candidateWords, r'WorkQueue', 'candidateWords');
+  }
+
+  @override
+  WorkQueue rebuild(void Function(WorkQueueBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  WorkQueueBuilder toBuilder() => new WorkQueueBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is WorkQueue &&
+        crossword == other.crossword &&
+        locationsToTry == other.locationsToTry &&
+        badLocations == other.badLocations &&
+        candidateWords == other.candidateWords;
+  }
+
+  @override
+  int get hashCode {
+    var _$hash = 0;
+    _$hash = $jc(_$hash, crossword.hashCode);
+    _$hash = $jc(_$hash, locationsToTry.hashCode);
+    _$hash = $jc(_$hash, badLocations.hashCode);
+    _$hash = $jc(_$hash, candidateWords.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper(r'WorkQueue')
+          ..add('crossword', crossword)
+          ..add('locationsToTry', locationsToTry)
+          ..add('badLocations', badLocations)
+          ..add('candidateWords', candidateWords))
+        .toString();
+  }
+}
+
+class WorkQueueBuilder implements Builder<WorkQueue, WorkQueueBuilder> {
+  _$WorkQueue? _$v;
+
+  CrosswordBuilder? _crossword;
+  CrosswordBuilder get crossword =>
+      _$this._crossword ??= new CrosswordBuilder();
+  set crossword(CrosswordBuilder? crossword) => _$this._crossword = crossword;
+
+  MapBuilder<Location, Direction>? _locationsToTry;
+  MapBuilder<Location, Direction> get locationsToTry =>
+      _$this._locationsToTry ??= new MapBuilder<Location, Direction>();
+  set locationsToTry(MapBuilder<Location, Direction>? locationsToTry) =>
+      _$this._locationsToTry = locationsToTry;
+
+  SetBuilder<Location>? _badLocations;
+  SetBuilder<Location> get badLocations =>
+      _$this._badLocations ??= new SetBuilder<Location>();
+  set badLocations(SetBuilder<Location>? badLocations) =>
+      _$this._badLocations = badLocations;
+
+  SetBuilder<String>? _candidateWords;
+  SetBuilder<String> get candidateWords =>
+      _$this._candidateWords ??= new SetBuilder<String>();
+  set candidateWords(SetBuilder<String>? candidateWords) =>
+      _$this._candidateWords = candidateWords;
+
+  WorkQueueBuilder();
+
+  WorkQueueBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _crossword = $v.crossword.toBuilder();
+      _locationsToTry = $v.locationsToTry.toBuilder();
+      _badLocations = $v.badLocations.toBuilder();
+      _candidateWords = $v.candidateWords.toBuilder();
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(WorkQueue other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$WorkQueue;
+  }
+
+  @override
+  void update(void Function(WorkQueueBuilder)? updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  WorkQueue build() => _build();
+
+  _$WorkQueue _build() {
+    _$WorkQueue _$result;
+    try {
+      _$result = _$v ??
+          new _$WorkQueue._(
+              crossword: crossword.build(),
+              locationsToTry: locationsToTry.build(),
+              badLocations: badLocations.build(),
+              candidateWords: candidateWords.build());
+    } catch (_) {
+      late String _$failedField;
+      try {
+        _$failedField = 'crossword';
+        crossword.build();
+        _$failedField = 'locationsToTry';
+        locationsToTry.build();
+        _$failedField = 'badLocations';
+        badLocations.build();
+        _$failedField = 'candidateWords';
+        candidateWords.build();
+      } catch (e) {
+        throw new BuiltValueNestedFieldError(
+            r'WorkQueue', _$failedField, e.toString());
       }
       rethrow;
     }
